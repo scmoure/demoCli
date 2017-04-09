e2da464a5b9eb275b361f6a317fdb10399128f60
Santiago Carretero <scarretero@emea.msad.sopra>
2017-04-09 13:32:56 +0200

    TSLint

--- src/app/project-form/project-form.component.ts
+++ src/app/project-form/project-form.component.ts
@@ -5,8 +5,6 @@ import { IComparable } from '../models/i.comparable.model';
 import { FormBuilder, FormGroup, AbstractControl} from '@angular/forms';
 import {Observable} from 'rxjs/Rx';
 
-
-
 @Component({
   selector: 'app-project-form',
   templateUrl: './project-form.component.html',
@@ -14,15 +12,15 @@ import {Observable} from 'rxjs/Rx';
 })
 export class ProjectFormComponent implements OnInit {
 
-  private dataList = new Array<Referentiel>(); 
-  private dataList2 = new Array<Referentiel>(); 
+  private dataList = new Array<Referentiel>();
+  private dataList2 = new Array<Referentiel>();
 
-  private selectValue : Referentiel = new Referentiel(2, 'R2', 'Referentiel 2');
-    private selectValue2 : Referentiel = new Referentiel(7, 'R7', 'Referentiel 7');
-  private formulaire : FormGroup;
-  private selectInput : AbstractControl;
-  
-  constructor(fb : FormBuilder) { 
+  private selectValue: Referentiel = new Referentiel(2, 'R2', 'Referentiel 2');
+  private selectValue2: Referentiel = new Referentiel(7, 'R7', 'Referentiel 7');
+  private formulaire: FormGroup;
+  private selectInput: AbstractControl;
+
+  constructor(fb: FormBuilder) {
     this.formulaire = fb.group({
       'selectInput' : [this.selectValue],
       'selectInput2' : [this.selectValue2]
@@ -31,17 +29,16 @@ export class ProjectFormComponent implements OnInit {
 
 
   ngOnInit() {
-    
-    for (let i : number = 1; i <= 3; i++) {
-      let referentiel : Referentiel = new Referentiel(i, `R${i}`, `Referentiel ${i}`);
-      let referentiel2 : Referentiel = new Referentiel(i+5, `R${i+5}`, `Referentiel ${i+5}`);
+    for (let i = 1; i <= 3; i++) {
+      let referentiel: Referentiel = new Referentiel(i, `R${i}`, `Referentiel ${i}`);
+      let referentiel2: Referentiel = new Referentiel(i+5, `R${i+5}`, `Referentiel ${i+5}`);
       this.dataList.push(referentiel);
       this.dataList2.push(referentiel2);
     }
 
   }
 
-  onSubmit() { 
+  onSubmit() {
     console.log(this.formulaire.value);
   }
 }
